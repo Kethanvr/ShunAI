b0ce605a2f04a2a69f1ebf83562a15c691c2f02e
kethan VR <kethanvr@gmail.com>
2025-04-23 01:10:14 +0530

    feat: add SVG icons for navigation and branding
    - Added `next.svg`, `vercel.svg`, and `window.svg` for UI components.
    - Introduced `favicon.ico` for the application.
    - Created global styles in `globals.css` with dark mode support.
    - Implemented layout structure in `layout.tsx` with font integration.
    - Developed homepage structure in `page.tsx` and `Homepage.tsx`.
    - Built a responsive navigation bar in `Navbar.tsx` with mobile support.
    - Added button component with placeholder functionality in `button.tsx`.
    - Created spotlight effect component in `spolight.tsx`.
    - Developed utility functions for class name management in `utils.ts`.
    - Configured TypeScript settings in `tsconfig.json`.

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Homepage from "@/components/Homepage";
+import { SpotlightPreview } from "@/components/spolight";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <main>
+  <SpotlightPreview />
+      
+   </main>
+  );
+}
